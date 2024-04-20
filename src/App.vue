347856852d5d2510f99d8e20cae94235de39c875
xinghe666666 <2838177922@qq.com>
2024-04-20 15:01:09 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <div class="AppMain">
+    <div>
+      <div class="App-logo">
+        <el-text>XingHe</el-text>
+      </div>
+      <div class="App-navigation">
+        <router-link to="/" class="App-navigation-router-link">首页</router-link>
+        <router-link to="/about">我</router-link>
+      </div>
+    </div>
+    <RouterView/>
+  </div>
+</template>
+
+<style>
+.AppMain{
+  width: 98vw;
+
+}
+.App-navigation{
+  float: right;
+}
+.App-navigation-router-link{
+  margin-right: 20px;
+}
+
+.App-logo {
+  float: left;
+}
+</style>
---
+++ src/views/homePage/homePageIndex.vue
@@ -0,0 +1,20 @@
+
+
+<template>
+  <div class="homePageIndexMain">
+    <div class="homePageIndexHead">
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+
+
+
+</style>
