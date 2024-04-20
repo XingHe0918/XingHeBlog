651b7aec80c38a461dd18f6d155ea0871cad2a1a
xinghe666666 <2838177922@qq.com>
2024-04-20 21:40:06 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
-
-
 <template>
-
   <div class="AppMain">
-    <div style="margin-top: 20px">
+    <div class="AppHead">
       <div class="App-logo">
-        <el-text style="color: white">XingHe</el-text>
+        <el-text style="color: white;font-size: 24px;font-weight: bold">XingHe</el-text>
       </div>
       <div class="App-navigation">
         <router-link to="/" class="App-navigation-router-link" style="color: white">首页</router-link>
@@ -15,32 +12,71 @@
     <RouterView/>
   </div>
 </template>
+
 <script setup lang="ts">
 </script>
-<style>
 
-.AppMain{
+<style scoped>
+.AppMain {
   width: 98vw;
-
   scrollbar-width: thin;
   overflow: hidden;
 }
-.AppMain::-webkit-scrollbar{
+
+.AppMain::-webkit-scrollbar {
   width: 8px;
 }
-.App-navigation{
+
+.App-navigation {
   float: right;
   display: flex;
-  align-items: center; /* 垂直居中 */
+  align-items: center;
 }
-.App-navigation-router-link{
+
+
+.App-navigation-router-link {
   margin-right: 40px;
   text-decoration: none;
+  color: white;
+  position: relative; /* 相对定位，以便下划线定位 */
+}
+
+.App-navigation-router-link:hover::after {
+  content: '';
+  position: absolute; /* 绝对定位，相对于父元素 */
+  left: 0;
+  bottom: -2px; /* 控制下划线位置 */
+  width: 100%; /* 宽度与文字元素一致 */
+  height: 2px; /* 下划线高度 */
+  background-color: white; /* 下划线颜色 */
+  animation: slideIn 0.5s forwards; /* 滑入动画 */
+}
+
+@keyframes slideIn {
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
 }
 
 .App-logo {
-  z-index: 9999; /* 设置 z-index 值，使其位于最上层 */
   float: left;
 }
+
+.AppHead {
+  height: 50px;
+  margin-top: 10px;
+  border-radius: 10px;
+  z-index: 9999;
+  background: rgba(0,0,0,0);
+  transition: background-color 0.5s ease-out;
+}
+.AppHead:hover{
+  background-color: rgba(0,0,0,0.5);
+}
+
+
 
 </style>
--- src/views/homePage/homePageIndex.vue
+++ src/views/homePage/homePageIndex.vue
@@ -8,7 +8,32 @@
     <div class="homePageIndexBody">
       <div class="left-section">
         <div class="homePageIndexUserInfo">
-
+          <div class="homePageIndexUserInfoHeadImage">
+            <img class="homePageIndexUserInfoHeadImage" src="@/image/head.png">
+          </div>
+          <div class="homePageIndexUserInfoBody">
+            <div class="homePageIndexUserInfoBodyName">
+              <h1>用户名</h1>
+            </div>
+            <div class="homePageIndexUserInfoBodyIntroduction">
+              <div>
+                <div>文章</div>
+                <div>100</div>
+              </div>
+              <div>
+                <div>点赞</div>
+                <div>100</div>
+              </div>
+              <div>
+                <div>收藏</div>
+                <div>100</div>
+              </div>
+              <div>
+                <div>粉丝</div>
+                <div>100</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div class="right-section">
@@ -28,7 +53,7 @@
   z-index: -1;
 }
 .homePageIndexBody{
-
+  margin-top: 30px;
 }
 .rounded-image {
   width: 98vw;
@@ -56,26 +81,59 @@
   border: #181818 solid 1px;
 }
 .homePageIndexUserInfo {
-  width: 400px; /* 宽度 */
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 350px; /* 宽度 */
   height: 400px; /* 高度 */
   background: linear-gradient(to right, #ff9966, #ff5e62); /* 渐变背景颜色 */
   border-radius: 5%; /* 圆形 */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5); /* 添加阴影 */
+  transition: box-shadow 0.3s ease; /* 添加过渡动画 */
 }
-@media (max-width: 768px) {
+.homePageIndexUserInfo:hover {
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.8); /* 鼠标悬停时的阴影效果 */
+}
+
+.homePageIndexUserInfoHeadImage {
+  width: 100px; /* 宽度 */
+  height: 100px; /* 高度 */
+  background: white;
+  border-radius: 50%; /* 圆形 */
+}
+.homePageIndexUserInfoHeadImage img {
+  transition: transform 0.5s ease;
+}
+.homePageIndexUserInfoHeadImage:hover img {
+  transform: rotate(360deg);
+  transition: transform 0.5s ease; /* 添加过渡动画 */
+}
+
+.homePageIndexUserInfoBody {
+  text-align: center;
+  float: none;
+}
+@media (max-width: 850px) {
   .left-section,
   .right-section {
     display: flex;
     justify-content: center; /* 水平居中 */
     overflow: hidden; /* 避免内容溢出 */
     border: #181818 solid 1px;
-    width: 100%;
+    width: 98%;
     float: none;
   }
 
   .homePageIndexUserInfo {
     width: 100%; /* 宽度占满 */
-    height: auto; /* 自适应高度 */
-    border-radius: 0; /* 取消圆角 */
   }
 }
+.homePageIndexUserInfoBodyIntroduction{
+  display: flex;
+  justify-content: space-around;
+  gap: 50px;
+}
+
+
 </style>
